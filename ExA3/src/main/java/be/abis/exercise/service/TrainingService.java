8527cedb4b0349ee6abe08193209f4971989a9ed
Siegfried Smeets <siegfried.smeets@bnpparibasfortis.com>
2020-10-09 10:49:45 +0200

    Fixed ExA3 missing AbisTrainingService

--- ExA3/src/main/java/be/abis/exercise/service/TrainingService.java
+++ ExA3/src/main/java/be/abis/exercise/service/TrainingService.java
@@ -2,6 +2,7 @@ package be.abis.exercise.service;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -13,12 +14,19 @@ import be.abis.exercise.model.Person;
 @Service
 public interface TrainingService {
 
+	 ArrayList<Person> getAllPersons();
 	 public Person findPerson(int id);
 	 Person findPerson(String emailAddress, String passWord);
 	 void addPerson(Person p) throws IOException;
-	 public List<Course> showFollowedCourses(Person person);
+	 
+	 public void deletePerson(int id);
 	 public void changePassword(Person p, String newPswd) throws IOException;
 	 
-	 public void enrollForSession(Person person, Course course, LocalDate date) throws EnrollException;
-	 
+	
+
+	 public List<Course> showFollowedCourses(Person person);
+	public void enrollForSession(Person person, Course course, LocalDate date) throws EnrollException;
+		
+	public CourseService getCourseService();
+	public void setCourseService(CourseService courseService);
 }
